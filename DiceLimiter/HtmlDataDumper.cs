7e795ef35eaeaa3f7694fa1e3560745ed30b6b56
Barrett Simms <wbsimms@gmail.com>
2013-04-20 11:13:47 -0400

    add html formatter

---
+++ DiceLimiter/HtmlDataDumper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceLimiter
+{
+    public class HtmlDataDumper
+    {
+        StringBuilder sb = new StringBuilder();
+
+        private string basestring =
+            "<table><thead><tr><th>Technology</th><th>Count</th></tr></thead><tbody>{0}</tbody></table>";
+        public HtmlDataDumper()
+        {
+        }
+
+        public void AddRow(string technology, int count)
+        {
+            sb.AppendFormat("<tr><td>{0}</td><td>{1}</td></tr>\r\n",technology,count);
+        }
+
+        public string GetHtmlData()
+        {
+            return string.Format(basestring, sb.ToString());
+        }
+    }
+}
--- DiceLimiter/Program.cs
+++ DiceLimiter/Program.cs
@@ -9,15 +9,21 @@ namespace DiceLimiter
 {
     public class Program
     {
-        public static void Main()
+        private static HtmlDataDumper htmlDataDumper = null; 
+
+        public static void Main(string[] args)
         {
-            string[] technologies = new[] {"jquery", ".NET", "C#", "java", "cobol", "javascript","perl","python","ruby","c++","objective-c","php","wordpress","node.js","knockout.js","VB"};
+            string[] technologies = new[] { "jquery", ".NET", "C#", "java", "cobol", "javascript", "perl", "python", "ruby", "c++", "objective-c", "php", "wordpress", "node.js", "knockout.js", "VB" };
+            if (args.Count() >= 0 && args[0] == "html") htmlDataDumper = new HtmlDataDumper();
+
             DiceLimit dl = new DiceLimit(); 
             foreach (string technology in technologies)
             {
                 int count = dl.GetDiceCount(technology);
+                if (htmlDataDumper != null) htmlDataDumper.AddRow(technology,count);
                 Console.WriteLine(technology+"\t"+count);
             }
+            if (htmlDataDumper != null) Console.WriteLine(htmlDataDumper.GetHtmlData());
         }
     }
 }
